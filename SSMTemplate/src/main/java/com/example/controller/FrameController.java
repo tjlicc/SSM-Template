140afdffbdfc5ce14312f2d9253729dc0fd08c7f
tj-lcc <604972736@qq.com>
2016-06-05 20:49:39 +0800

    添加定时任务

--- SSMTemplate/src/main/java/com/example/controller/FrameController.java
+++ SSMTemplate/src/main/java/com/example/controller/FrameController.java
@@ -1,10 +1,16 @@
 package com.example.controller;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FrameController {
+	
+	@Scheduled(cron="0/5 * *  * * ? ")
+	public void printMsg() {
+		System.out.println("testa");
+	}
 	
 	@RequestMapping(value="/index")
 	public String doIndex() {
